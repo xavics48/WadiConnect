0e003aeecbcaac7c801afb7cf27f5da77c266924
Abdul-Sami001 <abdulsami.kh001@gmail.com>
2024-12-11 19:00:31 +0500

    implemented signals, cutomer profile created, unique email

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,29 @@
 from django.db import models  # For model definitions
 from django.contrib.auth.models import AbstractUser  # For customizing the User model
 from django.conf import settings  # For referencing AUTH_USER_MODEL
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        extra_fields.setdefault("username", email.split("@")[0])  # Set default username if not provided
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
@@ -16,7 +39,9 @@ class CustomUser(AbstractUser):
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default=CUSTOMER)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()  # Set the custom manager
 
     def __str__(self):
         return f"{self.email} ({self.role})"
