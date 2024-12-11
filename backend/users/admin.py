268ee417ae89adb2be3e92c791a320226bbc88e6
Abdul-Sami001 <abdulsami.kh001@gmail.com>
2024-12-11 19:04:49 +0500

    added customer and seller profile to admin panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import CustomerProfile, SellerProfile
+
+admin.site.register(CustomerProfile)
+admin.site.register(SellerProfile)
+
